6be39fbc9bd83bf7855cce045f20ab98ff70f691
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-10-06 16:30:13 +0200

    [Dev] (main) Implent first main()
    Co-authored-by: damien-br <damien-br@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <string>
+
+#include "ANSI.h"
+#include "utilitaire.hpp"
+#include "Lexique.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    Lexique lexique_LesMiserables = Lexique("Lexique_LesMiserables");
+    std::string file_path = "../../tp1-Lexique-fichiers/lesMiserables_A.txt";
+    std::string file_content;
+
+    std::cout << "Reading file " << ANSI_CYAN << file_path << ANSI_RESET << std::endl;
+    if(!util::readFileIntoString(file_path, file_content))
+    {
+        std::cout << "Error reading file" << std::endl;
+        return -1;
+    }
+    std::cout << "File read successfully" << std::endl;
+
+    std::cout << "Lowering file content...";
+    util::to_lower(file_content);
+    std::cout << ANSI_GREEN << "DONE" << ANSI_RESET << std::endl;
+
+    std::cout << "Creating lexique from file content...";
+    lexique_LesMiserables.load_from_string(file_content);
+    std::cout << ANSI_GREEN << "DONE" << ANSI_RESET << std::endl;
+
+    std::cout << std::endl;
+    int unique_words_count = lexique_LesMiserables.get_unique_words_count();
+    std::cout << "Unique words count: " << unique_words_count << std::endl;
+
+    std::cout << lexique_LesMiserables << std::endl;
+    
     return 0;
 }
